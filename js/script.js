4ef56aab507a3dc333c6a98d85c6bb3f3bd86cda
Nathaniel Boonzaaijer <nbones23@gmail.com>
2021-03-25 17:17:04 -0700

    T-Shirt Info Section Complete
    Finished the t-shirt section, working on the activities section.

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', (e) => {
     document.getElementById('name').focus();
 
     // set the "other" job role option to be hidden by default:
-    otherJobRoleDisplay = document.getElementById('other-job-role').style.display;
-    otherJobRoleDisplay = 'none';
+    otherJobRoleDisplay = document.getElementById('other-job-role');
+    otherJobRoleDisplay.style.display = 'none';
 
     // Job Role Section:
     // display the other job role option only if the correct option is selected:  
@@ -13,35 +13,59 @@ document.addEventListener('DOMContentLoaded', (e) => {
     const jobOptionList = document.querySelector('#title');
 
     jobOptionList.addEventListener('change', (e) => {
-        console.log(e.target.value);
         if (e.target.value === 'other') {
-            otherJobRoleDisplay = 'list-item';
-        } else if (e.target.value != 'other' && otherJobRoleDisplay == 'list-item') {
-            otherJobRoleDisplay = 'none';
-        }
-        
+            otherJobRoleDisplay.style.display = 'list-item';
+        } else if (e.target.value != 'other' && otherJobRoleDisplay.style.display == 'list-item') {
+            otherJobRoleDisplay.style.display = 'none';
+        }   
     });
 
     // T-Shirt Info Section:
-    // only allow the user to see/choose a color until design has been chosen:
+	// only allow the user to see/choose a color until design has been chosen:
     shirtColorSelect = document.getElementById('shirt-colors');
     shirtColorSelect.style.display = 'none';
-
-    // listen for design select, and display the color select, 
-    //  with an available color option selected
-    const shirtDesigns = document.getElementById('shirt-designs');
+	// listen for design select, and display the color select, 
+        //  with an available color option selected
+	const shirtDesigns = document.getElementById('design');
+	const colorOptions = document.getElementById('color');
 
     shirtDesigns.addEventListener('change', (e) => {
-        // only display colors associated with the selected design:
-        for(let i=0;i<6;i++) {
-            if (shirtDesigns[i].data-theme == 'js puns') {
-                console.log('JS puns selected');
-            } else {
-                console.log('I love JS selected');
-            }
+		shirtColorSelect.style.display = 'list-item';
+        
+		// only display colors associated with the selected design:
+        if(e.target.value === 'js puns') {
+        	console.log('JS Puns selected!');
+			colorOptions[1].style.display = 'list-item';
+            colorOptions[2].style.display = 'list-item';
+			colorOptions[3].style.display = 'list-item';
+            colorOptions[4].style.display = 'none';
+            colorOptions[5].style.display = 'none';
+			colorOptions[6].style.display = 'none';
+        } else if(e.target.value === 'heart js') {
+			console.log('I love JS selected!');
+            colorOptions[1].style.display = 'none';
+            colorOptions[2].style.display = 'none';
+			colorOptions[3].style.display = 'none';
+            colorOptions[4].style.display = 'list-item';
+            colorOptions[5].style.display = 'list-item';
+			colorOptions[6].style.display = 'list-item';
+        }
+    });  
+
+    // Register for Activities Section:
+    // listen for changes in "activities" fieldset
+    const activities = document.getElementById('activities-box');
+    let totalPrice = document.getElementById('activites-cost').innerHTML;
+
+    activities.addEventListener(checked, (e) => {
+        console.log('Change event heard.');
+        if(e.target.name = 'all') {
+            
+            console.log(totalPrice);
+        } else {
+            
+            console.log(totalPrice);
         }
     });
 
-    
-    
 });
